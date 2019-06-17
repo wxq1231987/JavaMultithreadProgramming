169155eaf0d285e0a33c4a130fcf7b2d36b728fb
wxq1231987 <13272497851@163.com>
2019-06-17 20:16:17 +0800

    完成代码复制。

--- JavaMultithreadedProgramming/src/FileTransfer.java
+++ JavaMultithreadedProgramming/src/FileTransfer.java
@@ -1,31 +1,53 @@
+import java.io.BufferedInputStream;
+import java.io.BufferedOutputStream;
 import java.io.File;
-import java.util.regex.Pattern;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
 
 public class FileTransfer {
-    private static final String UNCONTAIN_DIR = "bin";
+    private static final String BIN_DIR = "bin";
+    private static final String SETTINGS_DIR = ".settings";
     private static final String CONTAIN_JAR = "java";
-	public static void main(String[] args) {
-		Pattern pattern = Pattern.compile("")
-		String srcPath = "C:\\Users\\Tina\\OneDrive\\Documents\\"
-				+ "\\Java高级开发工程师成长之路\\《Java多线程编程核心技术》源代码（高洪岩）50206\\src\\";
-		String dstPath = "E:\\wangxuqin\\JavaMultithreadProgramming\\";
-	    File src = new File(srcPath);
+    private static String srcPath = "C:\\Users\\Tina\\OneDrive\\Documents\\"
+					+ "\\Java高级开发工程师成长之路\\《Java多线程编程核心技术》源代码（高洪岩）50206\\src\\";
+    private static String dstPath = "E:\\wangxuqin\\JavaMultithreadProgramming\\";
+	public static void main(String[] args) throws IOException {
+		File src = new File(srcPath);
 	    File dst = new File(dstPath);
-	    if (src.isDirectory()) {
-	    	File[] fileArr = src.listFiles();
-	    	for (File file : fileArr) {
-	    		String currentFileName = file.getName();
-	    		if (file.isDirectory() && !UNCONTAIN_DIR.equals(currentFileName)) {
-	    		    File dstFile = new File(dst, currentFileName);
-	    		    if (!dstFile.exists()) {
-	    		    	dstFile.mkdir();
-	    		    }
-	    		}
-	    	}
-	    }
+	    transferFile(src, dst);
 	}
-	public static void transferFile(String srcPath, String dstPath) {
-		
+	public static void transferFile(File src, File dst) throws IOException {
+		String currentFileName = src.getName();
+	    if (src.isDirectory() && !BIN_DIR.equals(currentFileName) && !SETTINGS_DIR.equals(currentFileName)) {
+	    	dst = new File(dst, currentFileName);
+	    	if (!dst.exists()) {
+	    		dst.mkdir();
+		    }
+	    	File[] files = src.listFiles();
+	    	for (File file : files) {
+	    		transferFile(file, dst);
+	    	}
+	    } else if (currentFileName.endsWith(CONTAIN_JAR)){
+	    	dst = new File(dst, src.getName());
+	    	if (!dst.exists()) {
+	    		dst.createNewFile();
+	    	}
+	    	FileInputStream fis = new FileInputStream(src);
+	    	FileOutputStream fos = new FileOutputStream(dst);
+	    	BufferedInputStream bis = new BufferedInputStream(fis);
+	    	BufferedOutputStream bos = new BufferedOutputStream(fos);
+	    	byte[] buf = new byte[1024];
+	    	int readLength = -1;
+	    	while ((readLength = bis.read(buf)) != -1) {
+	    		bos.write(buf, 0, readLength);
+	    		bos.flush();
+	    	}
+	    	bis.close();
+	    	bos.close();
+	    	fos.close();
+	    	fis.close();
+	    }
 	}
 
 }
